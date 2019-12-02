52ed11d270b07a0de5c9bcc090752d3da97023e8
Guo Liang <msn103125@126.com>
2019-12-02 22:29:03 +0800

    add windows bat

---
+++ Gen.bat
@@ -0,0 +1,7 @@
+python2 pyang.py -f xtypedef CertusNet_Yang/GNB/cell/certus-5gnr-du-cell-base@2019-06-20.yang -p CertusNet_Yang/GNB/cell  -o CertusNet_Yang/GNB/output/gnb_du_oam_agent_rcfd_cell_base.h
+
+python2 pyang.py -f xtypedef CertusNet_Yang/GNB/cell/certus-5gnr-du-cell-channel@2019-06-20.yang -p CertusNet_Yang/GNB/cell  -o CertusNet_Yang/GNB/output/gnb_du_oam_agent_rcfd_cell_channel.h
+
+python2 pyang.py -f xtypedef CertusNet_Yang/GNB/cell/certus-5gnr-du-cell-l1@2019-06-20.yang -p CertusNet_Yang/GNB/cell  -o CertusNet_Yang/GNB/output/gnb_du_oam_agent_rcfd_cell_l1.h
+
+python2 pyang.py -f xtypedef CertusNet_Yang/GNB/cell/certus-5gnr-du-cell-schd@2019-06-20.yang -p CertusNet_Yang/GNB/cell  -o CertusNet_Yang/GNB/output/gnb_du_oam_agent_rcfd_cell_schd.h
