c92e9acf5b7527b558c41dd4262bdc0e23534eae
guoliang <msn103125@126.com>
2020-05-11 21:05:44 +0800

    update yangtool result

--- Gen_ORAN_Schema_xml.sh
+++ Gen_ORAN_Schema_xml.sh
@@ -2,21 +2,25 @@
 # pyang -f yin -o oran_yang/All_yin/o-ran-test.yin oran_yang/CommonModels/Operations/o-ran-test.yang \
 #       -p oran_yang/ImportedModels
 
-python pyang.py -f moxml --moXML-version=1.0 \
+python pyang.py -f moxml --moXML-version=1.1 \
                    oran_yang/ImportedModels/ietf-interfaces.yang \
                 -o oran_yang/All_Template/ietf-interfaces.xml
 
-python pyang.py -f moxml --moXML-version=1.0 \
+python pyang.py -f moxml --moXML-version=1.1 \
                    oran_yang/CommonModels/Operations/o-ran-operations.yang \
                 -o oran_yang/All_Template/o-ran-operations.xml
 
-python pyang.py -f moxml --moXML-version=1.0 \
+python pyang.py -f moxml --moXML-version=1.1 \
                    oran_yang/RUSpecificModels/Operations/o-ran-uplane-conf.yang \
                 -o oran_yang/All_Template/o-ran-uplane-conf.xml
 
-python pyang.py -f moxml --moXML-version=1.0 \
+python pyang.py -f moxml --moXML-version=1.1 \
                    oran_yang/CommonModels/Interfaces/o-ran-mplane-int.yang \
                 -o oran_yang/All_Template/o-ran-mplane-int.xml
+
+python pyang.py -f moxml --moXML-version=1.1 \
+                   oran_yang/CommonModels/Operations/o-ran-software-management.yang \
+                -o oran_yang/All_Template/o-ran-software-management.xml
 
 
 cp oran_yang/All_Template/*.xml ../../modules/rru_manager/data_schema
